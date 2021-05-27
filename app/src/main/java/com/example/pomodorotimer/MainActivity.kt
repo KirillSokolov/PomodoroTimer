68813ebf939432d6186c7a250372ac18a04bcc23
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2021-05-27 19:47:10 -0300

    Adding layout and simple CountDownTimer

--- app/src/main/java/com/example/pomodorotimer/MainActivity.kt
+++ app/src/main/java/com/example/pomodorotimer/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.pomodorotimer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.text.format.DateFormat
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pomodorotimer.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun test(view: View) {
+        var mTextField = binding.standardTime
+        val simpleDateFormat = SimpleDateFormat("mm:ss")
+
+        object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                var date = millisUntilFinished
+                var dateText = simpleDateFormat.format(date)
+                mTextField.setText(dateText)
+            }
+
+            override fun onFinish() {
+
+            }
+
+        }.start()
+
+
     }
 }
