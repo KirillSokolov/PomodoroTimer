c71cd1cc188553dd8e7fc1bd985b90ec949ca258
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2021-05-30 14:38:59 -0300

    Adding feature start stop and reset time

--- app/src/main/java/com/example/pomodorotimer/MainActivity.kt
+++ app/src/main/java/com/example/pomodorotimer/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.text.format.DateFormat
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pomodorotimer.databinding.ActivityMainBinding
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    var continueTimer = true
+    var miliTimer: Long = 0
+    var isInit = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,19 +28,40 @@ class MainActivity : AppCompatActivity() {
         var mTextField = binding.standardTime
         val simpleDateFormat = SimpleDateFormat("mm:ss")
 
-        object : CountDownTimer(30000, 1000) {
+        var timer: Long = 60 * 25 * 1000
+
+        if (isInit) {
+            timer = miliTimer
+        }
+
+        Toast.makeText(applicationContext, "Starting one Pomodoro", Toast.LENGTH_SHORT).show()
+
+        continueTimer = true
+
+        val count = object : CountDownTimer(timer, 1000) {
             override fun onTick(millisUntilFinished: Long) {
+                isInit = true
+                miliTimer = millisUntilFinished
                 var date = millisUntilFinished
                 var dateText = simpleDateFormat.format(date)
                 mTextField.setText(dateText)
+                if (!continueTimer) {
+                    cancel()
+                }
             }
-
             override fun onFinish() {
-
+                mTextField.setText("00:00")
             }
-
         }.start()
+    }
 
+    fun cancelTimer(view: View) {
+        continueTimer = false
+    }
 
+    fun resetTimer(view: View) {
+        cancelTimer(view)
+        isInit = false
+        binding.standardTime.setText("25:00")
     }
 }
