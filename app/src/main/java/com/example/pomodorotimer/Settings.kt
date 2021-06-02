4dcebf704f5408e6aab892b717b7396659e27bd5
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2021-06-02 13:02:45 -0300

    Adding NumberPicker function for settings

--- app/src/main/java/com/example/pomodorotimer/Settings.kt
+++ app/src/main/java/com/example/pomodorotimer/Settings.kt
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.View
+import android.widget.NumberPicker
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.get
 import com.example.pomodorotimer.databinding.ActivityMainBinding
 import com.example.pomodorotimer.databinding.ActivitySettingsBinding
 import kotlin.math.absoluteValue
@@ -20,15 +22,33 @@ class Settings : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        var pomodoroValue: NumberPicker = binding.pomodoroTime
+        pomodoroValue.setMinValue(1);
+        pomodoroValue.setMaxValue(60);
+        pomodoroValue.setWrapSelectorWheel(true);
+        pomodoroValue.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+
+        var shortValue: NumberPicker = binding.shortTime
+        shortValue.setMinValue(1);
+        shortValue.setMaxValue(60);
+        shortValue.setWrapSelectorWheel(true);
+        shortValue.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+
+        var longValue: NumberPicker = binding.longTime
+        longValue.setMinValue(1);
+        longValue.setMaxValue(60);
+        longValue.setWrapSelectorWheel(true);
+        longValue.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+
         val pref = PreferenceManager.getDefaultSharedPreferences(this)
         pref.apply {
             val pomodoro = getInt("1", 25)
             val shortTime = getInt("2", 5)
             val longTime = getInt("3", 10)
 
-            binding.pomodoroTime.setText(pomodoro.toString())
-            binding.shortTime.setText(shortTime.toString())
-            binding.longTime.setText(longTime.toString())
+            binding.pomodoroTime.value = pomodoro
+            binding.shortTime.value = shortTime
+            binding.longTime.value = longTime
 
             var checked = getBoolean("CHECKED", false)
             binding.darkTheme.isChecked = checked
@@ -40,9 +60,9 @@ class Settings : AppCompatActivity() {
         val pref = PreferenceManager.getDefaultSharedPreferences(this)
         val editor = pref.edit()
 
-        var pomodoroTimeGet = binding.pomodoroTime.text.toString().toInt()
-        var shortTimeGet = binding.shortTime.text.toString().toInt()
-        var longTimeGet = binding.longTime.text.toString().toInt()
+        var pomodoroTimeGet = binding.pomodoroTime.value
+        var shortTimeGet = binding.shortTime.value
+        var longTimeGet = binding.longTime.value
 
         editor
             .putInt("1", pomodoroTimeGet)
